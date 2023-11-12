35872269648fb05e209dd86437ff18d56c787526
Lord Turmoil <tony-turmoil@outlook.com>
2023-11-12 11:27:47 +0800

    update: index view

--- Planner/Views/IndexView.xaml.cs
+++ Planner/Views/IndexView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace Planner.Views;
 
@@ -10,5 +15,27 @@ public partial class IndexView : UserControl
     public IndexView()
     {
         InitializeComponent();
+    }
+
+    private void UIElement_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        if (e == null)
+        {
+            throw new ArgumentNullException(nameof(e));
+        }
+
+        var scv = (ScrollViewer)sender;
+        scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+        e.Handled = true;
+    }
+
+    private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+    {
+        Hyperlink link = sender as Hyperlink;
+        var psi = new ProcessStartInfo {
+            FileName = link.NavigateUri.AbsoluteUri,
+            UseShellExecute = true
+        };
+        Process.Start(psi);
     }
 }
